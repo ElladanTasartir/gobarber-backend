e65f3b469c709d617d9541136e18106240e4dcfe
Erick Malta <erickmalta100@gmail.com>
2020-08-30 00:22:40 -0300

    feat: :ok_hand: add created_at and updated_at to appointments table

--- src/database/migrations/1598413593330-CreateAppointments.ts
+++ src/database/migrations/1598413593330-CreateAppointments.ts
@@ -22,6 +22,16 @@ export default class CreateAppointments1598413593330
             name: 'date',
             type: 'timestamp with time zone',
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
