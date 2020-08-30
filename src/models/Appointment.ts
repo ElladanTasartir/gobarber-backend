2e525381eee6b115148577181305985a634ab136
Erick Malta <erickmalta100@gmail.com>
2020-08-30 00:26:37 -0300

    feat: :ok_hand: add created_at and updated_at to appointments model

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('appointments')
 class Appointment {
@@ -10,6 +16,12 @@ class Appointment {
 
   @Column('timestamp with time zone')
   date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
