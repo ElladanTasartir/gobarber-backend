c198b8a32bfb7287ce0555e0a0ef408540d82cb8
Erick Malta <erickmalta100@gmail.com>
2020-08-30 00:16:05 -0300

    fix: :wrench: removed isNullable key since it defaults to true

--- src/database/migrations/1598413593330-CreateAppointments.ts
+++ src/database/migrations/1598413593330-CreateAppointments.ts
@@ -17,12 +17,10 @@ export default class CreateAppointments1598413593330
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
         ],
       }),
