e78e7ffee7be45ec585bbc3a2d8863fe4c850556
Erick Malta <erickmalta100@gmail.com>
2020-08-30 17:21:27 -0300

    feat: :ok_hand: add encrypting to password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 
 import User from '../models/User';
 
@@ -18,7 +19,13 @@ class CreateUserService {
       throw new Error('Email address already used');
     }
 
-    const user = usersRepository.create({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     await usersRepository.save(user);
 
