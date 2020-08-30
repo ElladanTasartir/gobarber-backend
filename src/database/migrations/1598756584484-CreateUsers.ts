02e4838289ff963bd09002195347a9aafbd0d0ab
Erick Malta <erickmalta100@gmail.com>
2020-08-30 14:18:40 -0300

    fix: :wrench: changed id type to uuid - postgres only

--- src/database/migrations/1598756584484-CreateUsers.ts
+++ src/database/migrations/1598756584484-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1598756584484 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
