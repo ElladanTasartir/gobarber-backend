7dd87f886c205c3a5bc0db916bdbf36741ba3723
Erick Malta <erickmalta100@gmail.com>
2020-08-31 15:27:53 -0300

    feat: :ok_hand: add multer config

---
+++ src/config/upload.ts
@@ -0,0 +1,18 @@
+import { resolve } from 'path';
+import multer from 'multer';
+import crypto from 'crypto';
+
+const tmpFolder = resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+  directory: tmpFolder,
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
