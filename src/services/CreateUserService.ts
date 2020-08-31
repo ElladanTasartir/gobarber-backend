a9b0ddf7c494ef0cb17525b4a25a5e8d399a6f72
Erick Malta <erickmalta100@gmail.com>
2020-08-31 18:00:42 -0300

    feat: :ok_hand: add AppError instance to throw

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
 import User from '../models/User';
+import AppError from '../errors/AppError';
 
 interface Request {
   name: string;
@@ -16,7 +17,7 @@ class CreateUserService {
     const checkUserExists = await usersRepository.findOne({ where: { email } });
 
     if (checkUserExists) {
-      throw new Error('Email address already used');
+      throw new AppError('Email address already used');
     }
 
     const hashedPassword = await hash(password, 8);
--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 
 import uploadConfig from '../config/upload';
 import User from '../models/User';
+import AppError from '../errors/AppError';
 
 interface Request {
   user_id: string;
@@ -16,7 +17,8 @@ class UpdateUserAvatarService {
 
     const user = await usersRepository.findOne(user_id);
 
-    if (!user) throw new Error('Only authenticated users can change avatar');
+    if (!user)
+      throw new AppError('Only authenticated users can change avatar', 401);
 
     if (user.avatar) {
       const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
