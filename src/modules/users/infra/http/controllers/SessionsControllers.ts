170ed03dd489a07c0219668fd2ac89c6a8d2f19d
Erick Malta <erickmalta100@gmail.com>
2020-09-24 01:14:45 -0300

    feat: :ok_hand: add UserControllers

---
+++ src/modules/users/infra/http/controllers/SessionsControllers.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+
+export default class SessionsController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const authenticateUser = container.resolve(AuthenticateUserService);
+
+    const { user, token } = await authenticateUser.execute({ email, password });
+
+    const formattedUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json({ user: formattedUser, token });
+  }
+}
---
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateUserService from '@modules/users/services/CreateUserService';
+
+export default class UsersControllers {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = container.resolve(CreateUserService);
+
+    const user = await createUser.execute({ name, email, password });
+
+    const formattedUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.status(201).json(formattedUser);
+  }
+}
