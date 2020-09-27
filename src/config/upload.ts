d14142a84627c638bdf6a16db32c0c3f0631d98f
Erick Malta <erickmalta100@gmail.com>
2020-09-27 02:30:01 -0300

    feat: :ok_hand: add uploadsFolder

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -6,6 +6,7 @@ const tmpFolder = resolve(__dirname, '..', '..', 'tmp');
 
 export default {
   directory: tmpFolder,
+  uploadsFolder: resolve(tmpFolder, 'uploads'),
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename(request, file, callback) {
