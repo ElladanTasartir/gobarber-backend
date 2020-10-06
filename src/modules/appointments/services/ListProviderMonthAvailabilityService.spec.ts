82ab584223e7a40fdc3db0f713aaaac9e0621e22
Erick Malta <erickmalta100@gmail.com>
2020-10-06 01:25:03 -0300

    test: :construction: add ListProviderMonthAvailabilityService tests

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,63 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProvidersMonthAvailability: ListProviderMonthAvailabilityService;
+
+describe('ListProviderMonthAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProvidersMonthAvailability = new ListProviderMonthAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the month availability from provider', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 3, 20, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 10, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 8, 0, 0),
+    });
+
+    const availability = listProvidersMonthAvailability.execute({
+      provider_id: 'user',
+      month: 5,
+      year: 2020,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        {
+          day: 19,
+          available: true,
+        },
+        {
+          day: 20,
+          available: false,
+        },
+        {
+          day: 21,
+          available: false,
+        },
+        {
+          day: 22,
+          available: true,
+        },
+      ]),
+    );
+  });
+});
