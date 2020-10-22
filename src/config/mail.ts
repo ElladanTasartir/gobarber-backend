147757620beaeb435bd3b85e00b4baf961cd0647
Erick Malta <erickmalta100@gmail.com>
2020-10-22 00:37:56 -0300

    chore: :books: add mailConfig

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'erick@gobarber.com.br',
+      name: 'Erick Demori Malta',
+    },
+  },
+} as IMailConfig;
