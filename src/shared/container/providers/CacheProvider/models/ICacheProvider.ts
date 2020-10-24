1f47294533d8019d7648b42eff88e61cb1592f28
Erick Malta <erickmalta100@gmail.com>
2020-10-24 17:45:24 -0300

    feat: :ok_hand: add CacheProvider

---
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -0,0 +1,5 @@
+export default interface ICacheProvider {
+  save<T>(key: string, value: T): Promise<void>;
+  recover<T>(key: string): Promise<T | null>;
+  invalidate(key: string): Promise<void>;
+}
