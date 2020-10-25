5bba7a5cc1b50c684cb709cdceac23882977f481
Erick Malta <erickmalta100@gmail.com>
2020-10-25 02:42:55 -0300

    feaet: :ok_hand: add invalidatePrefix method

--- src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -2,4 +2,5 @@ export default interface ICacheProvider {
   save<T>(key: string, value: T): Promise<void>;
   recover<T>(key: string): Promise<T | null>;
   invalidate(key: string): Promise<void>;
+  invalidatePrefix(prefix: string): Promise<void>;
 }
