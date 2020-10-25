2ceda68d84a859a5409886fe3da977f2ac239826
Erick Malta <erickmalta100@gmail.com>
2020-10-25 18:46:07 -0300

    fix: :wrench: removed parseISO

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -10,13 +9,11 @@ export default class AppointmentsController {
 
     const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
-
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
       provider_id,
-      date: parsedDate,
+      date,
       user_id,
     });
 
